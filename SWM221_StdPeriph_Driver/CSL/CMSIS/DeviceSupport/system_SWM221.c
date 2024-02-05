6c9f4324be599c21531d4fecaa4c736e797899f4
XIVN1987 <XIVN1987@163.com>
2024-02-05 15:33:05 +0800

    initial commit

---
+++ SWM221_StdPeriph_Driver/CSL/CMSIS/DeviceSupport/system_SWM221.c
@@ -0,0 +1,276 @@
+/****************************************************************************************************************************************** 
+* 文件名称:	system_SWM221.c
+* 功能说明:	SWM221单片机的时钟设置
+* 技术支持:	http://www.synwit.com.cn/e/tool/gbook/?bid=1
+* 注意事项:
+* 版本日期: V1.0.0		2016年1月30日
+* 升级记录: 
+*
+*
+*******************************************************************************************************************************************
+* @attention
+*
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS WITH CODING INFORMATION 
+* REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME. AS A RESULT, SYNWIT SHALL NOT BE HELD LIABLE 
+* FOR ANY DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE CONTENT 
+* OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING INFORMATION CONTAINED HEREIN IN CONN-
+* -ECTION WITH THEIR PRODUCTS.
+*
+* COPYRIGHT 2012 Synwit Technology
+*******************************************************************************************************************************************/ 
+#include <stdint.h>
+#include "SWM221.h"
+
+
+/******************************************************************************************************************************************
+ * 系统时钟设定
+ *****************************************************************************************************************************************/
+#define SYS_CLK_8MHz		0	 	//内部高频8MHz RC振荡器
+#define SYS_CLK_1MHz		1		//内部高频1MHz RC振荡器
+#define SYS_CLK_XTAL		2		//外部晶体振荡器（8-24MHz）
+#define SYS_CLK_XTAL_DIV8	3		//外部晶体振荡器（8-24MHz） 8分频
+#define SYS_CLK_PLL			4		//锁相环输出
+#define SYS_CLK_PLL_DIV8	5		//锁相环输出 8分频
+#define SYS_CLK_32KHz		6		//内部低频32KHz RC  振荡器
+
+#define SYS_CLK   SYS_CLK_PLL
+
+
+#define __HSI		( 8000000UL)		//高速内部时钟
+#define __LSI		(   32000UL)		//低速内部时钟
+#define __HSE		(12000000UL)		//高速外部时钟
+#define __LSE		(   32000UL)		//低速外部时钟
+
+
+/********************************** PLL 设定 **********************************************
+ * VCO输出频率 = PLL输入时钟 / INDIV * 4 * FBDIV
+ * PLL输出频率 = PLL输入时钟 / INDIV * 4 * FBDIV / OUTDIV = VCO输出频率 / OUTDIV         
+ *****************************************************************************************/ 
+#define SYS_PLL_SRC   	SYS_CLK_XTAL	//可取值SYS_CLK_8MHz、SYS_CLK_XTAL
+
+#define PLL_IN_DIV		3
+
+#define PLL_FB_DIV		30
+
+
+#define PLL_OUT_DIV8	0
+#define PLL_OUT_DIV4	1
+#define PLL_OUT_DIV2	2
+
+#define PLL_OUT_DIV		PLL_OUT_DIV8
+
+
+uint32_t SystemCoreClock  = __HSI;   				//System Clock Frequency (Core Clock)
+uint32_t CyclesPerUs      = (__HSI / 1000000); 		//Cycles per micro second
+
+
+/****************************************************************************************************************************************** 
+* 函数名称: 
+* 功能说明: This function is used to update the variable SystemCoreClock and must be called whenever the core clock is changed
+* 输    入: 
+* 输    出: 
+* 注意事项: 
+******************************************************************************************************************************************/
+void SystemCoreClockUpdate(void)    
+{
+	if(SYS->CLKSEL & SYS_CLKSEL_SYS_Msk)			//SYS  <= HRC
+	{
+		SystemCoreClock = __HSI;
+	}
+	else											//SYS  <= CLK
+	{
+		switch((SYS->CLKSEL & SYS_CLKSEL_CLK_Msk) >> SYS_CLKSEL_CLK_Pos)
+		{
+		case 0:
+			SystemCoreClock = __LSI;
+			break;
+		
+		case 1:
+			if(SYS->PLLCR & SYS_PLLCR_INSEL_Msk)		//PLL_IN <= HRC
+			{
+				SystemCoreClock = __HSI;
+			}
+			else										//PLL_IN <= XTAL
+			{
+				SystemCoreClock = __HSE;
+			}
+			
+			SystemCoreClock = SystemCoreClock / PLL_IN_DIV * PLL_FB_DIV * 4 / (2 << (2 - PLL_OUT_DIV));
+			break;
+		
+		case 3:
+			SystemCoreClock = __HSE;
+			break;
+		
+		case 4:
+			SystemCoreClock = __HSI;
+			break;
+		}
+		
+		if(SYS->CLKSEL & SYS_CLKSEL_CLK_DIVx_Msk)  SystemCoreClock /= 8;
+	}
+	
+	CyclesPerUs = SystemCoreClock / 1000000;
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称: 
+* 功能说明: The necessary initializaiton of systerm
+* 输    入: 
+* 输    出: 
+* 注意事项: 
+******************************************************************************************************************************************/
+void SystemInit(void)
+{
+	SYS->CLKEN0 |= (1 << SYS_CLKEN0_ANAC_Pos);
+	
+	Flash_Param_at_xMHz(90);
+	
+	switch(SYS_CLK)
+	{
+		case SYS_CLK_8MHz:
+			switchTo8MHz();
+			break;
+		
+		case SYS_CLK_1MHz:
+			switchTo1MHz();
+			break;
+		
+		case SYS_CLK_XTAL:
+			switchToXTAL(0);
+			break;
+		
+		case SYS_CLK_XTAL_DIV8:
+			switchToXTAL(1);
+			break;
+		
+		case SYS_CLK_PLL:
+			switchToPLL(0);
+			break;
+		
+		case SYS_CLK_PLL_DIV8:
+			switchToPLL(1);
+			break;
+
+		case SYS_CLK_32KHz:
+			switchTo32KHz();
+			break;
+	}
+	
+	SystemCoreClockUpdate();
+	
+	Flash_Param_at_xMHz(SystemCoreClock/1000000);
+	
+	FMC->CACHE = (1 << FMC_CACHE_CEN_Pos) | (1 << FMC_CACHE_CCLR_Pos);	// 清除 Cache
+	__NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();
+	FMC->CACHE = (1 << FMC_CACHE_CEN_Pos) | (1 << FMC_CACHE_CPEN_Pos);
+	
+	PORTB->PULLD &= ~((1 << PIN10) | (1 << PIN11));
+	PORTB->PULLU &= ~((1 << PIN12) | (1 << PIN14));
+}
+
+void switchTo8MHz(void)
+{
+	SYS->RCCR |= (1 << SYS_RCCR_HON_Pos);
+	
+	SYS->CLKSEL |= (1 << SYS_CLKSEL_SYS_Pos);		//SYS <= HRC
+}
+
+void switchTo1M5Hz(void)
+{
+	switchTo8MHz();
+	
+	SYS->CLKDIVx_ON = 1;
+	
+	SYS->CLKSEL &= ~SYS_CLKSEL_CLK_Msk;
+	SYS->CLKSEL |= (4 << SYS_CLKSEL_CLK_Pos);		//CLK <= HRC
+
+	SYS->CLKSEL |= (1 << SYS_CLKSEL_CLK_DIVx_Pos);
+	
+	SYS->CLKSEL &=~(1 << SYS_CLKSEL_SYS_Pos);		//SYS <= HRC/8
+}
+
+void switchToXTAL(uint32_t div8)
+{
+	uint32_t i;
+	
+	switchTo8MHz();
+	
+	PORTB->PULLU &= ~((1 << PIN11) | (1 << PIN12));
+	PORTB->PULLD &= ~((1 << PIN11) | (1 << PIN12));
+	PORT_Init(PORTB, PIN11, PORTB_PIN11_XTAL_IN,  0);
+	PORT_Init(PORTB, PIN12, PORTB_PIN12_XTAL_OUT, 0);
+	SYS->XTALCR |= (1 << SYS_XTALCR_ON_Pos) | (1 << SYS_XTALCR_DET_Pos);
+	for(i = 0; i < 1000; i++) __NOP();
+	
+	SYS->CLKDIVx_ON = 1;
+	
+	SYS->CLKSEL &= ~SYS_CLKSEL_CLK_Msk;
+	SYS->CLKSEL |= (3 << SYS_CLKSEL_CLK_Pos);		//CLK <= XTAL
+
+	if(div8) SYS->CLKSEL |= (1 << SYS_CLKSEL_CLK_DIVx_Pos);
+	else     SYS->CLKSEL &=~(1 << SYS_CLKSEL_CLK_DIVx_Pos);
+	
+	SYS->CLKSEL &=~(1 << SYS_CLKSEL_SYS_Pos);		//SYS <= XTAL
+}
+
+void switchToPLL(uint32_t div8)
+{
+	switchTo8MHz();
+	
+	PLLInit();
+	
+	SYS->CLKDIVx_ON = 1;
+	
+	SYS->CLKSEL &= ~SYS_CLKSEL_CLK_Msk;
+	SYS->CLKSEL |= (1 << SYS_CLKSEL_CLK_Pos);		//CLK <= PLL
+
+	if(div8)  SYS->CLKSEL |= (1 << SYS_CLKSEL_CLK_DIVx_Pos);
+	else      SYS->CLKSEL &=~(1 << SYS_CLKSEL_CLK_DIVx_Pos);
+	
+	SYS->CLKSEL &=~(1 << SYS_CLKSEL_SYS_Pos);		//SYS <= PLL
+}
+
+void switchTo32KHz(void)
+{
+	switchTo8MHz();
+	
+	SYS->RCCR |= (1 << SYS_RCCR_LON_Pos);
+	
+	SYS->CLKDIVx_ON = 1;
+	
+	SYS->CLKSEL &= ~SYS_CLKSEL_CLK_Msk;
+	SYS->CLKSEL |= (0 << SYS_CLKSEL_CLK_Pos);		//CLK <= LRC
+
+	SYS->CLKSEL &=~(1 << SYS_CLKSEL_CLK_DIVx_Pos);
+
+	SYS->CLKSEL &=~(1 << SYS_CLKSEL_SYS_Pos);		//SYS <= LRC
+}
+
+
+void PLLInit(void)
+{	
+	if(SYS_PLL_SRC == SYS_CLK_8MHz)
+	{		
+		SYS->RCCR |= (1 << SYS_RCCR_HON_Pos);
+	}
+	else if(SYS_PLL_SRC == SYS_CLK_XTAL)
+	{		
+		PORTB->PULLU &= ~((1 << PIN11) | (1 << PIN12));
+		PORTB->PULLD &= ~((1 << PIN11) | (1 << PIN12));
+		PORT_Init(PORTB, PIN11, PORTB_PIN11_XTAL_IN,  0);
+		PORT_Init(PORTB, PIN12, PORTB_PIN12_XTAL_OUT, 0);
+		SYS->XTALCR |= (1 << SYS_XTALCR_ON_Pos) | (1 << SYS_XTALCR_DET_Pos);
+	}
+	
+	SYS->PLLCR = (0 		<< SYS_PLLCR_PWRDN_Pos) |
+				 (1 		<< SYS_PLLCR_OUTEN_Pos) |
+				 ((SYS_PLL_SRC == SYS_CLK_XTAL) << SYS_PLLCR_INSEL_Pos) |
+				 (0 		 << SYS_PLLCR_BYPASS_Pos) |
+				 (PLL_IN_DIV << SYS_PLLCR_INDIV_Pos)  |
+				 (PLL_FB_DIV << SYS_PLLCR_FBDIV_Pos);
+	
+	while((SYS->PLLSR & SYS_PLLSR_LOCK_Msk) == 0) __NOP();		//等待PLL锁定
+	
+	SYS->PLLSR |= SYS_PLLSR_ENA_Msk;
+}
---
+++ SWM221_StdPeriph_Driver/CSL/SWM221_StdPeriph_Driver/SWM221_qspi.h
@@ -0,0 +1,192 @@
+#ifndef __SWM221_QSPI_H__
+#define __SWM221_QSPI_H__
+
+
+typedef struct {
+	uint16_t Size;			// Flash 大小，双 Flash 时指两个 Flash 的总大小，取值 QSPI_Size_1MB、...、QSPI_Size_512MB
+	uint8_t  Bank;			// 选择 SPI Flash 连接的 QSPI Bank，可取值 QSPI_Bank_1、QSPI_Bank_2、QSPI_Bank_Dual
+	uint16_t ClkDiv;		// 可取值 2--256
+	uint8_t  ClkMode;		// 可取值 QSPI_ClkMode_0、QSPI_ClkMode_3
+	uint8_t  SampleShift;	// 可取值 QSPI_SampleShift_None、QSPI_SampleShift_Half_SPICLK、QSPI_SampleShift_1_SYSCLK、...
+	uint8_t  IntEn;			// 可取值 QSPI_IT_ERR、QSPI_IT_DONE、QSPI_IT_FFTHR、QSPI_IT_PSMAT、QSPI_IT_TO 及其“或”
+} QSPI_InitStructure;
+
+#define QSPI_Size_1MB		19
+#define QSPI_Size_2MB		20
+#define QSPI_Size_4MB		21
+#define QSPI_Size_8MB		22
+#define QSPI_Size_16MB		23
+#define QSPI_Size_32MB		24
+#define QSPI_Size_64MB		25
+#define QSPI_Size_128MB		26
+#define QSPI_Size_256MB		27
+#define QSPI_Size_512MB		28
+
+#define QSPI_Bank_1			0
+#define QSPI_Bank_2			2
+#define QSPI_Bank_Dual		1
+
+#define QSPI_ClkMode_0		0
+#define QSPI_ClkMode_3		1
+
+#define QSPI_SampleShift_NONE						0x00000
+#define QSPI_SampleShift_1_SYSCLK					0x00001
+#define QSPI_SampleShift_2_SYSCLK					0x00010
+#define QSPI_SampleShift_3_SYSCLK					0x00011
+#define QSPI_SampleShift_4_SYSCLK					0x00100
+#define QSPI_SampleShift_5_SYSCLK					0x00101
+#define QSPI_SampleShift_6_SYSCLK					0x00110
+#define QSPI_SampleShift_7_SYSCLK					0x00111
+#define QSPI_SampleShift_Half_SPICLK				0x10000
+#define QSPI_SampleShift_Half_SPICLK_PLUS_1_SYSCLK	0x10001
+#define QSPI_SampleShift_Half_SPICLK_PLUS_2_SYSCLK	0x10010
+#define QSPI_SampleShift_Half_SPICLK_PLUS_3_SYSCLK	0x10011
+#define QSPI_SampleShift_Half_SPICLK_PLUS_4_SYSCLK	0x10100
+#define QSPI_SampleShift_Half_SPICLK_PLUS_5_SYSCLK	0x10101
+#define QSPI_SampleShift_Half_SPICLK_PLUS_6_SYSCLK	0x10110
+#define QSPI_SampleShift_Half_SPICLK_PLUS_7_SYSCLK	0x10111
+
+
+typedef struct {
+	uint8_t  Instruction;			// 指令码
+	uint8_t  InstructionMode;		// 可取值：QSPI_PhaseMode_None、QSPI_PhaseMode_1bit、QSPI_PhaseMode_2bit、QSPI_PhaseMode_4bit
+	uint32_t Address;
+	uint8_t  AddressMode;			// 可取值：QSPI_PhaseMode_None、QSPI_PhaseMode_1bit、QSPI_PhaseMode_2bit、QSPI_PhaseMode_4bit
+	uint8_t  AddressSize;			// 可取值：QSPI_PhaseSize_8bit、QSPI_PhaseSize_16bit、QSPI_PhaseSize_24bit、QSPI_PhaseSize_32bit
+	uint32_t AlternateBytes;
+	uint8_t  AlternateBytesMode;	// 可取值：QSPI_PhaseMode_None、QSPI_PhaseMode_1bit、QSPI_PhaseMode_2bit、QSPI_PhaseMode_4bit
+	uint8_t  AlternateBytesSize;	// 可取值：QSPI_PhaseSize_8bit、QSPI_PhaseSize_16bit、QSPI_PhaseSize_24bit、QSPI_PhaseSize_32bit
+	uint8_t  DummyCycles;			// 可取值：0--31
+	uint8_t  DataMode;				// 可取值：QSPI_PhaseMode_None、QSPI_PhaseMode_1bit、QSPI_PhaseMode_2bit、QSPI_PhaseMode_4bit
+	uint32_t DataCount;				// 要读写数据的字节个数，0 表示一直读写直到存储器末尾
+	uint8_t  SendInstOnlyOnce;
+} QSPI_CmdStructure;
+
+#define QSPI_PhaseMode_None		0	// there is no this phase
+#define QSPI_PhaseMode_1bit		1	// 单线传输
+#define QSPI_PhaseMode_2bit		2	// 双线传输
+#define QSPI_PhaseMode_4bit		3	// 四线传输
+
+#define QSPI_PhaseSize_8bit		0
+#define QSPI_PhaseSize_16bit	1
+#define QSPI_PhaseSize_24bit	2
+#define QSPI_PhaseSize_32bit	3
+
+
+#define QSPI_Mode_IndirectWrite		0
+#define QSPI_Mode_IndirectRead		1
+#define QSPI_Mode_AutoPolling		2
+#define QSPI_Mode_MemoryMapped		3
+
+
+#define QSPI_CMD_READ_JEDEC			0x9F
+#define QSPI_CMD_FAST_READ			0x0B
+#define QSPI_CMD_FAST_READ_2bit		0x3B
+#define QSPI_CMD_FAST_READ_IO2bit	0xBB
+#define QSPI_CMD_FAST_READ_4bit		0x6B
+#define QSPI_CMD_FAST_READ_IO4bit	0xEB
+#define QSPI_CMD_WRITE_ENABLE		0x06
+#define QSPI_CMD_WRITE_DISABLE		0x04
+#define QSPI_CMD_PAGE_PROGRAM		0x02
+#define QSPI_CMD_PAGE_PROGRAM_4bit	0x32
+#define QSPI_CMD_ERASE_CHIP			0x60
+#define QSPI_CMD_ERASE_SECTOR 		0x20
+#define QSPI_CMD_ERASE_BLOCK32KB	0x52
+#define QSPI_CMD_ERASE_BLOCK64KB	0xD8
+#define QSPI_CMD_READ_STATUS_REG1	0x05
+#define QSPI_CMD_READ_STATUS_REG2	0x35
+#define QSPI_CMD_READ_STATUS_REG3	0x15
+#define QSPI_CMD_WRITE_STATUS_REG1	0x01
+#define QSPI_CMD_WRITE_STATUS_REG2	0x31
+#define QSPI_CMD_WRITE_STATUS_REG3	0x11
+#define QSPI_CMD_WRITE_EXT_ADDR		0xC5	// Write Extended Address Register
+#define QSPI_CMD_READ_EXT_ADDR		0xC8
+#define QSPI_CMD_4BYTE_ADDR_ENTER	0xB7
+#define QSPI_CMD_4BYTE_ADDR_EXIT	0xE9
+
+#define QSPI_STATUS_REG1_BUSY_Pos	0
+#define QSPI_STATUS_REG2_QUAD_Pos	1
+
+
+/* Interrupt Type */
+#define QSPI_IT_ERR   	(1 << QSPI_CR_ERR_Pos)
+#define QSPI_IT_DONE   	(1 << QSPI_CR_DONE_Pos)
+#define QSPI_IT_FFTHR	(1 << QSPI_CR_FFTHR_Pos)
+#define QSPI_IT_PSMAT	(1 << QSPI_CR_PSMAT_Pos)
+#define QSPI_IT_TO		(1 << QSPI_CR_TOIE_Pos)
+
+
+
+void QSPI_Init(QSPI_TypeDef * QSPIx, QSPI_InitStructure * initStruct);
+void QSPI_Open(QSPI_TypeDef * QSPIx);
+void QSPI_Close(QSPI_TypeDef * QSPIx);
+
+void QSPI_CmdStructClear(QSPI_CmdStructure * cmdStruct);
+void QSPI_Command(QSPI_TypeDef * QSPIx, uint8_t cmdMode, QSPI_CmdStructure * cmdStruct);
+
+void QSPI_Erase(QSPI_TypeDef * QSPIx, uint8_t cmd, uint32_t addr, uint8_t wait);
+void QSPI_Write_(QSPI_TypeDef * QSPIx, uint32_t addr, uint8_t buff[], uint32_t count, uint8_t data_width);
+#define QSPI_Write(QSPIx, addr, buff, count)	   QSPI_Write_(QSPIx, (addr), (buff), (count), 1)
+#define QSPI_Write_4bit(QSPIx, addr, buff, count)  QSPI_Write_(QSPIx, (addr), (buff), (count), 4)
+void QSPI_Read_(QSPI_TypeDef * QSPIx, uint32_t addr, uint8_t buff[], uint32_t count, uint8_t addr_width, uint8_t data_width);
+#define QSPI_Read(QSPIx, addr, buff, count)			QSPI_Read_(QSPIx, (addr), (buff), (count), 1, 1);
+#define QSPI_Read_2bit(QSPIx, addr, buff, count)	QSPI_Read_(QSPIx, (addr), (buff), (count), 1, 2);
+#define QSPI_Read_4bit(QSPIx, addr, buff, count)	QSPI_Read_(QSPIx, (addr), (buff), (count), 1, 4);
+#define QSPI_Read_IO2bit(QSPIx, addr, buff, count)	QSPI_Read_(QSPIx, (addr), (buff), (count), 2, 2);
+#define QSPI_Read_IO4bit(QSPIx, addr, buff, count)	QSPI_Read_(QSPIx, (addr), (buff), (count), 4, 4);
+
+void QSPI_MemoryMap(QSPI_TypeDef * QSPIx, uint8_t addr_width, uint8_t data_width);
+void QSPI_MemoryMapClose(QSPI_TypeDef * QSPIx);
+
+bool QSPI_FlashBusy(QSPI_TypeDef * QSPIx);
+uint8_t QSPI_QuadState(QSPI_TypeDef * QSPIx);
+void QSPI_QuadSwitch(QSPI_TypeDef * QSPIx, uint8_t on);
+void QSPI_SendCmd(QSPI_TypeDef * QSPIx, uint8_t cmd);
+uint32_t QSPI_ReadReg(QSPI_TypeDef * QSPIx, uint8_t cmd, uint8_t n_bytes);
+void QSPI_WriteReg(QSPI_TypeDef * QSPIx, uint8_t cmd, uint32_t data, uint8_t n_bytes);
+
+#define QSPI_ReadJEDEC(QSPIx)			QSPI_ReadReg(QSPIx, QSPI_CMD_READ_JEDEC, 3)
+#define QSPI_WriteEnable(QSPIx)			QSPI_SendCmd(QSPIx, QSPI_CMD_WRITE_ENABLE)
+#define QSPI_WriteDisable(QSPIx)		QSPI_SendCmd(QSPIx, QSPI_CMD_WRITE_DISABLE)
+#define QSPI_4ByteAddrEnable(QSPIx)		QSPI_SendCmd(QSPIx, QSPI_CMD_4BYTE_ADDR_ENTER)
+#define QSPI_4ByteAddrDisable(QSPIx)	QSPI_SendCmd(QSPIx, QSPI_CMD_4BYTE_ADDR_EXIT)
+
+
+static inline bool QSPI_Busy(QSPI_TypeDef * QSPIx)
+{
+	return QSPIx->SR & QSPI_SR_BUSY_Msk;
+}
+
+static inline uint32_t QSPI_FIFOCount(QSPI_TypeDef * QSPIx)
+{
+	return (QSPIx->SR & QSPI_SR_FFLVL_Msk) >> QSPI_SR_FFLVL_Pos;
+}
+
+static inline uint32_t QSPI_FIFOSpace(QSPI_TypeDef * QSPIx)
+{
+	return 32 - QSPI_FIFOCount(QSPIx);
+}
+
+static inline bool QSPI_FIFOEmpty(QSPI_TypeDef * QSPIx)
+{
+	return QSPI_FIFOCount(QSPIx) == 0;
+}
+
+static inline void QSPI_BankSwitch(QSPI_TypeDef * QSPIx, uint8_t bank)
+{
+	QSPIx->CR &= ~(QSPI_CR_DUAL_Msk | QSPI_CR_BANK_Msk);
+	QSPIx->CR |=  (bank << QSPI_CR_DUAL_Pos);
+}
+
+static inline void QSPI_CacheClear(QSPI_TypeDef * QSPIx)
+{
+	QSPIx->CACHE = QSPI_CACHE_CLR_Msk;
+}
+
+void QSPI_INTEn(QSPI_TypeDef * QSPIx, uint32_t it);
+void QSPI_INTDis(QSPI_TypeDef * QSPIx, uint32_t it);
+void QSPI_INTClr(QSPI_TypeDef * QSPIx, uint32_t it);
+uint32_t QSPI_INTStat(QSPI_TypeDef * QSPIx, uint32_t it);
+
+
+#endif //__SWM221_QSPI_H__
