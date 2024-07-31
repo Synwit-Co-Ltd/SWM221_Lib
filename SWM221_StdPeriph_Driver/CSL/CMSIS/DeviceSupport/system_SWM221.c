7ee417a1e37dc6105d72966b97b635dc17447f54
XIVN1987 <XIVN1987@163.com>
2024-07-31 17:32:46 +0800

    remove Cache Predict Enable

--- SWM221_StdPeriph_Driver/CSL/CMSIS/DeviceSupport/system_SWM221.c
+++ SWM221_StdPeriph_Driver/CSL/CMSIS/DeviceSupport/system_SWM221.c
@@ -176,7 +176,6 @@ void SystemInit(void)
 	
 	FMC->CACHE = (1 << FMC_CACHE_CEN_Pos) | FMC_CACHE_CCLR_Msk;	// Çå³ý Cache
 	__NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();
-	FMC->CACHE = (1 << FMC_CACHE_CEN_Pos) | (1 << FMC_CACHE_CPEN_Pos);
 	
 	PORTB->PULLD &= ~((1 << PIN10) | (1 << PIN11));
 	PORTB->PULLU &= ~((1 << PIN12) | (1 << PIN15));
