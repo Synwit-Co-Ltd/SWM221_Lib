ea65122e39b41491c8d8f7195e4c56ad059c36f6
XIVN1987 <XIVN1987@163.com>
2024-08-20 15:51:24 +0800

    add IAP_Flash_Param_TAC() call

--- SWM221_StdPeriph_Driver/CSL/SWM221_StdPeriph_Driver/SWM221_flash.c
+++ SWM221_StdPeriph_Driver/CSL/SWM221_StdPeriph_Driver/SWM221_flash.c
@@ -25,9 +25,10 @@ typedef int  (*IAP_Flash_Erase_t)(uint32_t sector, uint32_t flag);
 typedef int  (*IAP_Flash_Write_t)(uint32_t flash_addr, uint32_t ram_addr, uint32_t count, uint32_t flag);
 
 
-IAP_Flash_Param_t IAP_Flash_Param = (IAP_Flash_Param_t)0x010004D1;
 IAP_Flash_Erase_t IAP_Flash_Erase = (IAP_Flash_Erase_t)0x01000401;
 IAP_Flash_Write_t IAP_Flash_Write = (IAP_Flash_Write_t)0x01000461;
+IAP_Flash_Param_t IAP_Flash_Param = (IAP_Flash_Param_t)0x010004D1;
+IAP_Flash_Param_t IAP_Flash_Param_TAC = (IAP_Flash_Param_t)0x010004F1;
 
 
 /****************************************************************************************************************************************** 
@@ -84,6 +85,8 @@ uint32_t FLASH_Write(uint32_t addr, uint32_t buff[], uint32_t count)
 void Flash_Param_at_xMHz(uint32_t x)
 {
 	__disable_irq();
+	
+	IAP_Flash_Param_TAC(30, 0x0B11FFAC);
 	
 	IAP_Flash_Param(1000 / x, 0x0B11FFAC);
 	
